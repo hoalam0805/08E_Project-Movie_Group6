7858e33debf9d88dbcd1be0057a988ad4ceedffc
lamnhuthoa <hoalamnhut@gmail.com>
2021-08-26 15:23:37 +0700

    setup tailwindcss and install some packages from npm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="shadow-sm ..." >Hello</div>
+        <div className="shadow ..." >Hello</div>
+        <div className="shadow-md ..." >Hello</div>
+        <div className="shadow-lg ..." >Hello</div>
+        <div className="shadow-xl ..." >Hello</div>
+        <div className="shadow-2xl ..." >Hello</div>
+      </div>
     </div>
   );
 }
